08f1cd636e84a847b5662c862ea7fc19c1bd2c5f
frknmydn <furkan.meydan@hotmail.com>
2023-09-07 01:22:53 +0300

    spring app initilazed with java 17 spring 2.7 and maven for post service

---
+++ Post-Service/src/main/java/com/furkanmeydan/PostService/PostServiceApplication.java
@@ -0,0 +1,15 @@
+package com.furkanmeydan.PostService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostServiceApplication.class, args);
+		System.out.println("Post Service is running...");
+
+	}
+
+}
